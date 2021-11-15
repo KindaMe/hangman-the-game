4e8ac2502becd6127503bf24d9e350d7e358f27f
Patryk Kalinowski <kalinowski1999@gmail.com>
2021-11-16 00:48:56 +0100

    Add randomWord function.

--- hangman-the-game/hangman-the-game.cpp
+++ hangman-the-game/hangman-the-game.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <random>
+#include <vector>
+
+std::string randomWord(std::vector<std::string> words);
 
 int main()
 {
-	std::cout << "HANGMAN - THE GAME";
+	std::vector<std::string> words;
+	srand((unsigned)time(0));
+
+	words.push_back("1111");
+	words.push_back("2222");
+	words.push_back("3333");
+
+	std::cout << randomWord(words) << "\n";
+	std::cout << randomWord(words) << "\n";
+	std::cout << randomWord(words) << "\n";
+	std::cout << randomWord(words) << "\n";
+	std::cout << randomWord(words) << "\n";
+	std::cout << randomWord(words) << "\n";
+	std::cout << randomWord(words) << "\n";
+}
+
+std::string randomWord(std::vector<std::string> words)
+{
+	int randomNumber = rand() % words.size();
+	return words[randomNumber];
 }
