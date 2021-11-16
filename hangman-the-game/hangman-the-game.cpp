e5f5cbab9bb54f2b5cc4f05e1e0fe8d1a211462c
Patryk Kalinowski <kalinowski1999@gmail.com>
2021-11-16 02:24:23 +0100

    Add vaildGuess function and clean up.

--- hangman-the-game/hangman-the-game.cpp
+++ hangman-the-game/hangman-the-game.cpp
@@ -5,6 +5,7 @@
 
 void gameplayLoop(std::vector<std::string> words);
 std::string randomWord(std::vector<std::string> words);
+bool validGuess(char guess, std::vector<char>wrongGuesses, std::vector<char>correctGuesses);
 
 int main()
 {
@@ -39,20 +40,40 @@ void gameplayLoop(std::vector<std::string> words)
 	int health = 6;
 	char guess;
 	std::vector<char>wrongGuesses;
+	std::vector<char>correctGuesses;
 	bool correctGuess = false;
 	int score = 0;
 
 	do
 	{
-		system("cls");
-		std::cout << "HANGMAN - THE GAME\n";
-		std::cout << "******************\n";
+		do
+		{
+			system("cls");
+			std::cout << "HANGMAN - THE GAME\n";
+			std::cout << "******************\n";
 
-		std::cout << wordBlank;
+			std::cout << wordBlank;
 
-		std::cout << "\n\nTries left: " << health;
-		std::cout << "\nEnter your guess: ";
-		std::cin >> guess;
+			std::cout << "\n\nTries left: " << health;
+			std::cout << "\nEnter your guess: ";
+			std::cin >> guess;
+
+			if (validGuess(guess, correctGuesses, wrongGuesses) == false)
+			{
+				system("cls");
+				std::cout << "HANGMAN - THE GAME\n";
+				std::cout << "******************\n";
+
+				std::cout << wordBlank;
+
+				std::cout << "\n\nTries left: " << health;
+
+				std::cout << "\nYou tried that one already!";
+				std::cin.clear();
+				std::cin.ignore();
+				std::cin.get();
+			}
+		} while (validGuess(guess, correctGuesses, wrongGuesses) == false);
 
 		for (int i = 0; i < length; i++)
 		{
@@ -63,35 +84,51 @@ void gameplayLoop(std::vector<std::string> words)
 				score++;
 			}
 		}
-		if (correctGuess == false)
+		switch (correctGuess)
 		{
+		case false:
 			wrongGuesses.push_back(guess);
 			health--;
-		}
-		else
-		{
+			break;
+		case true:
+			correctGuesses.push_back(guess);
 			correctGuess = false;
+			break;
 		}
 	} while (score != length && health != 0);
 
+	system("cls");
+	std::cout << "HANGMAN - THE GAME\n";
+	std::cout << "******************\n\n";
+
+	std::cout << word;
+
 	if (score == length)
 	{
-		system("cls");
-		std::cout << "HANGMAN - THE GAME\n";
-		std::cout << "******************\n\n";
-
-		std::cout << wordBlank;
-
 		std::cout << "\n\nYou win!";
 	}
 	else if (health == 0)
 	{
-		system("cls");
-		std::cout << "HANGMAN - THE GAME\n";
-		std::cout << "******************\n\n";
-
-		std::cout << wordBlank;
-
 		std::cout << "\n\nYou lose.";
 	}
+}
+
+bool validGuess(char guess, std::vector<char> wrongGuesses, std::vector<char> correctGuesses)
+{
+	for (int i = 0; i < correctGuesses.size(); i++)
+	{
+		if (correctGuesses[i] == guess)
+		{
+			return false;
+		}
+	}
+	for (int i = 0; i < wrongGuesses.size(); i++)
+	{
+		if (wrongGuesses[i] == guess)
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
